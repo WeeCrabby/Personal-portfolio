8936c42975c7784cbdf33206e4b389770a6fc966
WeeCrabby <67542282+WeeCrabby@users.noreply.github.com>
2023-07-11 16:40:26 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import MainCard from "@/components/MainCard.vue";
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <!--<sine-wave-animation></sine-wave-animation>-->
+    <MainCard/>
+
+  </main>
+</template>
+
+<style scoped>
+/*
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+
+  }
+
+  .
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}*/
+</style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,142 @@
+<script>
+import { ref } from 'vue'
+const name = ref('Vue.js')
+export default {
+  name: "MainCard",
+  methods: {
+    goto(refName) {
+      let element = this.$refs[refName];
+      let top = element.offsetTop;
+
+      window.scrollTo(0, top);
+    }
+  }
+
+}
+
+</script>
+
+<style scoped>
+.fade-enter-from{
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 3s ease;
+}
+
+.fade-slower-enter-from { opacity:0; }
+.fade-slower-enter-active {
+  transition: opacity 10s ease;
+}
+</style>
+
+<template>
+  <section class="dark">
+
+    <transition appear name="fade">
+      <h1>Welcome</h1>
+    </transition>
+    <transition appear name="fade-slower">
+      <a class="down-pointer" href="#intro_section">
+        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24" fill="none" stroke="#99ffd6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>
+      </a>
+    </transition>
+
+
+  </section>
+  <div class="main-container">
+    <section class="dark" id="intro_section">
+      <div class="intro" >
+        <h1>Lucie Svobodová</h1>
+        <h2>Front-end Developer</h2>
+        <p>Hi, I'm Lucie, a front-end developer based in Brno, Czech Republic. I have a passion for web development and love to create for web and mobile devices.</p>
+        <p>I'm currently looking for a new role as a front-end developer. If you're looking for a developer, please get in touch!</p>
+        <br>
+        <div>
+          <div class="socials">
+            <div class="link">
+              <img src="../assets/linkedin.png" alt="" height="50px" width="50px">
+              <a href="" target="_blank"></a>
+            </div>
+
+          </div>
+          <div class="socials">
+            <div class="link">
+              <img src="../assets/github.png" alt="" height="50px" width="50px">
+              <a href="" target="_blank"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="dark_aqua">
+      <div class="wave-up">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+          <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+        </svg>
+      </div>
+      <div class="about">
+        <h1>About me</h1>
+        <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+      </div>
+    </section>
+
+    <section class="dark">
+      <div class="wave-up">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="dark_aqua-fill"></path>
+          <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="dark_aqua-fill"></path>
+          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="dark_aqua-fill"></path>
+        </svg>
+      </div>
+      <div>
+        <h1>Technologies studied</h1>
+        <div class="tech-section">
+          <img class="tech-logo" src="../assets/css.png" alt="">
+          <img class="tech-logo" src="../assets/html-5.png" alt="">
+          <img class="tech-logo" src="../assets/js.png" alt="">
+          <img class="tech-logo" src="../assets/node-js.png" alt="">
+          <img class="tech-logo" src="../assets/vue.png" alt="">
+        </div>
+      </div>
+    </section>
+
+    <section class="dark_aqua">
+      <div class="wave-up">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+          <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+        </svg>
+      </div>
+      <div class="resume_projects_section">
+        <h1>Projects</h1>
+        <!-- TO DO: add projects -->
+      </div>
+    </section>
+
+    <section class="mint">
+      <div class="wave-up mint">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="dark_aqua-fill"></path>
+          <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="dark_aqua-fill"></path>
+          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="dark_aqua-fill"></path>
+        </svg>
+      </div>
+      <footer>
+        <a href="https://www.flaticon.com/free-icons/instagram" title="instagram icons">Instagram icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by Smashicons - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/html" title="html icons">Html icons created by Pixel perfect - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/css" title="css icons">Css icons created by Pixel perfect - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons">Javascript icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/node-js" title="node js icons">Node js icons created by Freepik - Flaticon</a>
+      </footer>
+    </section>
+
+  </div>
+</template>
