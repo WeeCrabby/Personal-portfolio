610f2e51a0c39ee058de64e890f45abb00ba42a9
WeeCrabby <67542282+WeeCrabby@users.noreply.github.com>
2023-07-21 14:00:13 +0200

    Sinewave and projects section
    added the sinewave animation, styled the projects section

--- src/App.vue
+++ src/App.vue
@@ -9,31 +9,12 @@ import MainCard from "@/components/MainCard.vue";
   </header>
 
   <main>
-    <!--<sine-wave-animation></sine-wave-animation>-->
+
     <MainCard/>
 
   </main>
 </template>
 
 <style scoped>
-/*
-header {
-  line-height: 1.5;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-
-  }
-
-  .
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}*/
 </style>
--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,16 +1,90 @@
 <script>
-import { ref } from 'vue'
-const name = ref('Vue.js')
+import * as dat from 'dat.gui';
 export default {
+  mounted() {
+    //add sinewave script
+    const canvas = document.querySelector('canvas');
+    const gui = new dat.GUI();
+    dat.GUI.toggleHide();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    class Wave {
+      constructor(
+          canvas,
+          maxAmplitude = 100,
+          length = 100,
+          frequency = 8,
+          bgOpacity = 0.03,
+          y,
+      ) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
+        this.maxAmplitude = maxAmplitude;
+        this.amplitude = 0;
+        this.length = length;
+        this.frequency = frequency;
+        this.increment = Math.random() * 360;
+        this.bgOpacity = bgOpacity;
+        this.y = y || this.canvas.height / 2;
+
+        this.frameCallback = () => {
+          this.draw(this.ctx);
+          requestAnimationFrame(this.frameCallback);
+        };
+      }
+
+      draw(c) {
+        c.beginPath();
+
+        this.ctx.fillStyle = `rgba(0, 0, 0, ${this.bgOpacity})`;
+        this.ctx.strokeStyle = `#99ffd6`;
+
+        c.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        c.moveTo(0, this.canvas.height / 2);
+
+        for (let i = 0; i < this.canvas.width; i += 1) {
+          c.lineTo(
+              i,
+              this.y + Math.sin(i / this.length + this.increment) * this.amplitude,
+          );
+        }
+
+        c.stroke();
+        c.closePath();
+
+        this.amplitude = Math.sin(this.increment) * this.maxAmplitude;
+        this.increment -= this.frequency / 1000;
+      }
+
+      animate() {
+        this.frameCallback();
+      }
+    }
+
+    const wave = new Wave(canvas, 150, 200, -8);
+
+    gui.add(wave, 'maxAmplitude', 0, canvas.height / 2);
+    gui.add(wave, 'length', 0, canvas.width);
+    gui.add(wave, 'frequency', -50, 50);
+    gui.add(wave, 'bgOpacity', 0, 1);
+    gui.add(wave, 'y', 0, canvas.height);
+
+    wave.animate();
+
+
+  },
   name: "MainCard",
   methods: {
+
     goto(refName) {
       let element = this.$refs[refName];
       let top = element.offsetTop;
 
       window.scrollTo(0, top);
     }
-  }
+  },
 
 }
 
@@ -31,21 +105,28 @@ export default {
 </style>
 
 <template>
-  <section class="dark">
-
-    <transition appear name="fade">
-      <h1>Welcome</h1>
-    </transition>
-    <transition appear name="fade-slower">
-      <a class="down-pointer" href="#intro_section">
-        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24" fill="none" stroke="#99ffd6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>
-      </a>
-    </transition>
-
-
-  </section>
   <div class="main-container">
+    <section class="dark">
+      <canvas id="canvas"></canvas>
+      <div class="welcome">
+        <transition appear name="fade">
+          <h1>Welcome</h1>
+        </transition>
+        <transition appear name="fade-slower">
+          <a class="down-pointer" href="#intro_section">
+            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="112" viewBox="0 0 24 24" fill="none" stroke="#99ffd6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>
+          </a>
+        </transition>
+      </div>
+    </section>
     <section class="dark" id="intro_section">
+      <div class="wave-up">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+          <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+        </svg>
+      </div>
       <div class="intro" >
         <h1>Lucie Svobodová</h1>
         <h2>Front-end Developer</h2>
@@ -55,15 +136,16 @@ export default {
         <div>
           <div class="socials">
             <div class="link">
-              <img src="../assets/linkedin.png" alt="" height="50px" width="50px">
-              <a href="" target="_blank"></a>
+               <a href="https://github.com/WeeCrabby" target="_blank">
+                 <img src="../assets/linkedin.png" alt="" height="50px" width="50px">
+               </a>
             </div>
-
           </div>
           <div class="socials">
             <div class="link">
-              <img src="../assets/github.png" alt="" height="50px" width="50px">
-              <a href="" target="_blank"></a>
+                 <a href="https://github.com/WeeCrabby" target="_blank">
+                   <img src="../assets/github.png" alt="" height="50px" width="50px">
+                 </a>
             </div>
           </div>
         </div>
@@ -116,6 +198,18 @@ export default {
       <div class="resume_projects_section">
         <h1>Projects</h1>
         <!-- TO DO: add projects -->
+        <a href="" target="_blank" style="text-decoration:none;">
+          <div class="project">
+            <img class="project-img" src="../assets/github.png" alt="">
+            <div class="project-text">
+              <h2 class="project-header">Project 1</h2>
+              <p>Project description Project description Project description Project description Project description Project description Project description</p>
+            </div>
+          </div>
+
+        </a>
+
+
       </div>
     </section>
 
